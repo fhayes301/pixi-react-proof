0ecfda4cfbaa0c90ee80a302841d9f5eb1702882
Faye Hayes <fhayes30@gmail.com>
2017-12-21 11:46:57 -0800

    Add pixi.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var ReactPIXI = require('react-pixi');
+var PIXI = require('pixi.js');
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
     );
