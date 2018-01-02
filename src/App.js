88a63d4a34451757f7b5e651b96b79e5f848ea1c
Faye Hayes <fhayes30@gmail.com>
2018-01-02 14:21:53 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import {Stage, Texture, TilingSprite} from 'react-pixi';
+import logo from './logo.svg';
+const PIXI = require('pixi.js');
 
-var ReactPIXI = require('react-pixi');
-var PIXI = require('pixi.js');
 
-class App extends Component {
+var application = new PIXI.Application(800, 600, { transparent: true });
+document.body.appendChild(application.view);
+
+class App extends React.Component {
+
+
+  playVideo() {
+    var texture = PIXI.Texture.fromVideoURL('https://www.youtube.com/watch?v=2G8V00SkTvYd');
+    var videoSprite = new PIXI.Sprite(texture);
+    videoSprite.width = app.screen.width;
+    videoSprite.height = app.screen.height;
+    application.stage.addChild(videoSprite);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <p className="App-intro">
 
-        </p>
-      </div>
+    return (
+
+      { this.playVideo() }
+
     );
   }
-}
+};
 
 export default App;
