5d19c06f796ca18b9a2195808673056515ad518c
Faye Hayes <fhayes30@gmail.com>
2018-01-02 14:49:59 -0800

    debugging player

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
-import {Stage, Texture, TilingSprite} from 'react-pixi';
+import {Stage, Texture, Sprite, Screen, TilingSprite} from 'react-pixi';
 import logo from './logo.svg';
 const PIXI = require('pixi.js');
 
 
 var application = new PIXI.Application(800, 600, { transparent: true });
-document.body.appendChild(application.view);
 
 class App extends React.Component {
 
 
+
   playVideo() {
-    var texture = PIXI.Texture.fromVideoURL('https://www.youtube.com/watch?v=2G8V00SkTvYd');
+    var texture = PIXI.Texture.fromVideoUrl('https://www.youtube.com/watch?v=2G8V00SkTvYd');
+    console.log(texture)
     var videoSprite = new PIXI.Sprite(texture);
-    videoSprite.width = app.screen.width;
-    videoSprite.height = app.screen.height;
+    console.log(videoSprite)
+    videoSprite.width = application.screen.width;
+    console.log(videoSprite.width)
+    videoSprite.height = application.screen.height;
+    console.log(videoSprite.height)
     application.stage.addChild(videoSprite);
   }
 
   render() {
 
-    return (
 
-      { this.playVideo() }
+    document.body.appendChild(application.view);
 
-    );
+      return (
+        this.playVideo()
+      )
+
+
   }
 };
 
